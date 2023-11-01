71cb3e333a82c14002e9c6c94c1c04760e84d228
fupeei <fupeichris@gmail.com>
2023-10-31 22:55:42 -0300

    Juego de números Genial

---
+++ server.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)  
+app.secret_key = 'martina'
+
+numero_secreto = random.randint(0,100)
+
+
+
+@app.route('/jugar')         
+def index():
+    if 'numero_secreto' not in session:
+            session['numero_secreto'] = random.randint(1, 100)
+            print(session['numero_secreto'])
+    return render_template('index.html')
+
+
+@app.route("/respuesta", methods=["POST"])
+def respuesta():
+    
+    numero_adivinado = int(request.form['numero_respuesta'])
+    numero_secreto = session['numero_secreto']
+        
+    if numero_adivinado < numero_secreto:
+        mensaje = 'Demasiado bajo'
+    elif numero_adivinado > numero_secreto:
+        mensaje = 'Demasiado alto'
+    else:
+        mensaje = '¡Correcto! El número era ' + str(numero_secreto)
+        session.pop('numero_secreto')
+
+    return render_template('index.html', mensaje=mensaje)
+
+
+if __name__=="__main__":   
+    app.run(debug=True)
